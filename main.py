aac5f8a32ec64c1f77a985277606b0102352c1c0
frozenfirebomb <bartvrugt@hotmail.com>
2025-10-13 08:16:38 -0400

    working on having INV harm code compared to Steel harm code list

--- main.py
+++ main.py
@@ -17,10 +17,27 @@ while has_data:
     if data == None:
         has_data = False
 
-rows = inv_ws.iter_rows(min_row=1, max_row=inv_row_count-1, min_col=1, max_col=2)
+# rows = inv_ws.iter_rows(min_row=1, max_row=inv_row_count-1, min_col=1, max_col=2)
 
-for a,b in rows:
-    print(a.value, b.value)
+# for a,b in rows:
+#     print(a.value, b.value)
 
-print("")
-print("Number of rows: ",inv_row_count)
+# for i in range(2, inv_row_count):
+#     print(inv_ws.cell(row=i, column=6).value)
+
+def harm_codes(fp):        #returns the contents of a text file as a list of strings from a file path provided as a string
+    codes = []
+    with open(fp) as f:
+        file_contents = f.read()    
+    for content in file_contents.split():
+        codes.append(content)
+    return codes
+
+# for i in range(2, inv_row_count):
+#     if inv_ws.cell(row=i, column=6).value in harm_codes("/mnt/c/Users/Bart/Desktop/Harmonized Chapters/steelHTSlist_justnumbers.txt"):
+#         print("steel decleration required", inv_ws.cell(row=i, column=6).value)
+
+for i in range(2, inv_row_count):
+    for c in harm_codes("/mnt/c/Users/Bart/Desktop/Harmonized Chapters/steelHTSlist_justnumbers.txt"):
+        if c in inv_ws.cell(row=i, column=6).value:
+            print("steel decleration required", inv_ws.cell(row=i, column=6).value)
