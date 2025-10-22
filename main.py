15eb8a2048acd8a584dde7e1e8bf93a47b89a0fb
frozenfirebomb <bartvrugt@hotmail.com>
2025-10-22 07:53:11 -0400

    depacked a tuple of tuples into a single list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import openpyxl
 from openpyxl import Workbook
+from openpyxl.styles import Border
 
 inv_wb = openpyxl.load_workbook("/mnt/c/Users/Bart/Desktop/Harmonized Chapters/INVDAY_master.xlsx")
 inv_ws = inv_wb[ 'Sheet1']
@@ -13,7 +14,7 @@ final_ws = final_wb.active
 steel_codes = "/mnt/c/Users/Bart/Desktop/Harmonized Chapters/steelHTSlist_justnumbers.txt"
 alum_codes = "/mnt/c/Users/Bart/Desktop/Harmonized Chapters/aluminumHTSlist_justnumbers.txt"
 
-has_data = True
+has_data = True     # lines 16 - 23, used to find the number of rows in the inv sheet that contain data.
 inv_row_count = 0
 
 while has_data:
@@ -22,7 +23,7 @@ while has_data:
     if data == None:
         has_data = False
 
-has_data = True
+has_data = True     # lines 25-32, used to find the number of rows in the metal_master sheet that contain data.
 metal_master_row_count = 0
 
 while has_data:
@@ -58,14 +59,18 @@ def declared_sku(harm_cell):        # finds sku associated with harm requiring d
 steel_sku = declared_sku(find_declaration_req(harm_codes(steel_codes)))
 alum_sku = declared_sku(find_declaration_req(harm_codes(alum_codes)))
 
-print(steel_sku)
+print(metal_master_ws['A6'].border)
+
+# final_ws['A1'] = metal_master_ws['A1'].value
 
 for sku in steel_sku:
     for i in range(1, metal_master_row_count):
         value = metal_master_ws.cell(row=i, column=3).value
         if value == sku.value:
-            for cell1 in final_ws[f"A{i-1}" : f"H{i+2}"] metal_master_ws[f"A{i-1}" : f"H{i+2}"]:
-        
-            # print(metal_master_ws[f"A{i-1}" : f"H{i+2}"])
+            metal_master_section = metal_master_ws[f"A{i-1}" : f"H{i+2}"]
+            final_section = final_ws[f"A{i-1}" : f"H{i+2}"]
+            metal_master_cells = [val for row in metal_master_section for val in row]
+            for i in metal_master_cells:
+                print(i)
 
 final_wb.save("/mnt/c/Users/Bart/Desktop/Harmonized Chapters/final_test.xlsx")
